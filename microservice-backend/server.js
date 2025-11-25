1a7d2ca71ac9645fb6029cf69e90babe895aa9c6
yassinehriz1 <yassinehriz532@gmail.com>
2025-11-26 00:23:40 +0100

    remove grpc

--- microservice-backend/server.js
+++ microservice-backend/server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const client = require('prom-client');
-const { getLastConnectedUser, startGRPCClient } = require('./grpc-client');
 
 const app = express();
 const collectDefaultMetrics = client.collectDefaultMetrics;
@@ -13,13 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes);
 
-app.get('/api/current-user', (req, res) => {
-  const username = getLastConnectedUser();
-  if (!username) {
-    return res.status(404).json({ error: "Aucun utilisateur connecté pour le moment." });
-  }
-  res.json({ username });
-});
+
+
 
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
@@ -30,7 +24,6 @@ mongoose.connect("mongodb://admin:pass@mongodb-statefulset-0.mongo-service:27017
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection failed:', err));
 
-startGRPCClient();
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Catalogue service running on port ${process.env.PORT}`);
